57bc31248e735a39a051db5b8dfe10b358857e75
Pranav Chintala <pranav.chintala27@gmail.com>
2025-09-16 13:43:24 -0400

    reverted changes

--- students_submissions/gcd_pac27.py
+++ students_submissions/gcd_pac27.py
@@ -19,4 +19,3 @@ def gcd(a: int, b: int) -> int:
 print(gcd(54, 24))  # Expected output: 6
 print(gcd(48, 18))  # Expected output: 6
 print(gcd(101, 10))  # Expected output: 1
-#fix
--- students_submissions/gcd_sr2452.py
+++
@@ -1,4 +0,0 @@
-def gcd(a: int, b: int) -> int:
-    if b == 0:
-        return abs(a) 
-    return gcd(b, a % b)
